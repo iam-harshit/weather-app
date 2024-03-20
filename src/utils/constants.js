1429098fb4edc17d887f9fae2f253a076969694e
iam-harshit <importantemail545@gmail.com>
2024-03-20 17:35:22 +0530

    Add constants for API key and base URLs

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const API_KEY = "i0MdXaPBgNGFvrt5PfZVy5mMgD9Br56K";
+export const REALTIME_API_BASE_URL = "https://api.tomorrow.io/v4/weather/realtime";
+export const FORECAST_API_BASE_URL = "https://api.tomorrow.io/v4/weather/forecast";
